827e7b3876101d1c72ead518361da3a24fc96333
binhaoz <68266288+binhaoz@users.noreply.github.com>
2020-12-16 02:19:45 +1100

    format

--- 2D-ion-dnesity.py
+++ 2D-ion-dnesity.py
@@ -7,48 +7,52 @@ Created on Tue Aug  1 03:59:17 2017
 import numpy as np
 import os
 
-
-#data = open("atom.lammpstrj","r")
+# data = open("atom.lammpstrj","r")
 x = []
 atomnumber = 785
 frame = 550
+
+
 def atom_dis(r, skip=9):
     i = 1
     for i in range(skip):
         r.readline()
-        i +=1
+        i += 1
     i = 1
     for i in range(atomnumber):
-        i +=1
+        i += 1
         s = r.readline().strip().split()
-        #print(s)
+        # print(s)
         Type = int(s[1])
         xs = float(s[2]) % 1
         xe = float(s[3]) % 1
-        if Type ==7: 
-            x.append([xs,xe])
-            #print(Type)
-            #print(s)
-    return 
+        if Type == 7:
+            x.append([xs, xe])
+            # print(Type)
+            # print(s)
+    return
+
 
 def cycle(frame):
-    r = open("80-atom.lammpstrj","r")
+    r = open("80-atom.lammpstrj", "r")
     j = 1
     for j in range(frame):
-        j +=1
+        j += 1
         atom_dis(r, skip=9)
         x_array = np.array(x)
-        #z_array = x_array
-        #z_array += z_array
+        # z_array = x_array
+        # z_array += z_array
     return x_array
 
-#atomnum(file)
+
+# atomnum(file)
 cycle(frame)
-#hist2,bins2=np.histogram(cycle(frame=69),bins=100)
-#print(hist2)
-#print(bins2) 
-#test
-#test22222222
-#test33333333
-#test4
-#test5
+# hist2,bins2=np.histogram(cycle(frame=69),bins=100)
+# print(hist2)
+# print(bins2)
+# test
+# test22222222
+# test33333333
+# test4
+# test5
+# test6
